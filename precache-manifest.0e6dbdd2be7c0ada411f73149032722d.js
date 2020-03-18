28ab4fd12a41e2a1b03fecd4c805dfd962d9aeef
Ania Sniadek <sniadek97@gmail.com>
2020-03-18 13:25:52 +0100

    Updates

---
+++ precache-manifest.0e6dbdd2be7c0ada411f73149032722d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbbdaaf15e25c06144274f815aa843d",
+    "url": "//speedy09.github.io/to-do-app/index.html"
+  },
+  {
+    "revision": "13a6f9308813f21187e6",
+    "url": "//speedy09.github.io/to-do-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "457046b9c6d848a83de9",
+    "url": "//speedy09.github.io/to-do-app/static/css/main.398af477.chunk.css"
+  },
+  {
+    "revision": "13a6f9308813f21187e6",
+    "url": "//speedy09.github.io/to-do-app/static/js/2.36ab2cd3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "//speedy09.github.io/to-do-app/static/js/2.36ab2cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457046b9c6d848a83de9",
+    "url": "//speedy09.github.io/to-do-app/static/js/main.d7498606.chunk.js"
+  },
+  {
+    "revision": "0aa17aa3abc2cd57c682",
+    "url": "//speedy09.github.io/to-do-app/static/js/runtime-main.6e86c58b.js"
+  }
+]);
